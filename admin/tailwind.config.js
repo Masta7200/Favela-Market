f9337e0a5f408d4db9cfa9617399ff31ce7984ef
moustaphaab <moustaphaabakarex@gmail.com>
2025-12-31 10:51:10 +0100

    second commit

---
+++ admin/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+content: ["./index.html", "./src/**/*.{js,jsx}"],
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
